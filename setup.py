a699f5d8e22d483a661192f3c4381927c24e9e89
Ali Panahi <panaali@gmail.com>
2019-01-24 15:37:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='tensorboardcolab',
-    version='0.0.21.1a',
+    version='0.0.21.1b',
     packages=['tensorboardcolab'],
     url='https://github.com/taomanwai/tensorboardcolab',
     license='MIT',
